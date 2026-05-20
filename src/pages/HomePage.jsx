9e9909ed5c190e63ae71503524f6be401d871263
Spidey2342 <mygoals990@gmail.com>
2026-05-20 23:24:31 +0000

    check

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
-import Hero from "../components/home/Hero";
-import HowItWorks from "../components/home/HowItWorks";
-import LearningTracks from "../components/home/LearningTracks";
+import Hero from "../components/layout/Hero";
+import HowItWorks from "../components/layout/HowItWorks";
+import LearningTracks from "../components/layout/LearningTracks";
 
 export default function HomePage() {
   return (
